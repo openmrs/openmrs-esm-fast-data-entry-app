bf667e8972651be3c3b1ec5d4e7fd1453e46e05b
Zac Butko <zac.butko@gmail.com>
2022-08-30 07:44:03 -0700

    Patient serach + styling

--- src/add-group-modal/AddGroupModal.tsx
+++ src/add-group-modal/AddGroupModal.tsx
@@ -6,27 +6,60 @@ import {
   ModalFooter,
   ModalBody,
   TextInput,
+  FormLabel,
 } from "@carbon/react";
 import { Add } from "@carbon/react/icons";
 import { useTranslation } from "react-i18next";
+import { ExtensionSlot } from "@openmrs/esm-framework";
+import styles from "./styles.scss";
 
 const AddGroupModal = () => {
   const { t } = useTranslation();
   const [open, setOpen] = useState(false);
   const [name, setName] = useState("");
+  const [patientList, setPatientList] = useState([]);
+
+  const handleSelectPatient = (id) => {
+    setPatientList([...patientList, id]);
+  };
+
   return (
-    <>
+    <div className={styles.modal}>
       <Button onClick={() => setOpen(true)}>
         {t("createNewGroup", "Create new group")} <Add size={20} />
       </Button>
       <ComposedModal open={open} onClose={() => setOpen(false)}>
         <ModalHeader>{t("createNewGroup", "Create New Group")}</ModalHeader>
         <ModalBody>
-          <TextInput
-            labelText={t("newGroupName", "New Group Name")}
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              rowGap: "1rem",
+            }}
+          >
+            <TextInput
+              labelText={t("newGroupName", "New Group Name")}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <FormLabel>Patients in group</FormLabel>
+            <ul>
+              {patientList?.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+            <FormLabel>Search for patients to add to group</FormLabel>
+            <ExtensionSlot
+              extensionSlotName="patient-search-bar-slot"
+              state={{
+                selectPatientAction: handleSelectPatient,
+                buttonProps: {
+                  kind: "primary",
+                },
+              }}
+            />
+          </div>
         </ModalBody>
         <ModalFooter>
           <Button kind="secondary" onClick={() => setOpen(false)}>
@@ -37,7 +70,7 @@ const AddGroupModal = () => {
           </Button>
         </ModalFooter>
       </ComposedModal>
-    </>
+    </div>
   );
 };
 
