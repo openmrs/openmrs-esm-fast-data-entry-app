2345036526950ef78f28e793b0a399aa6d0ee481
Ian <52504170+ibacher@users.noreply.github.com>
2023-06-23 03:05:57 -0400

    (BREAKING) Migrate esm-fast-data-entry to use routes.json (#60)
    * (BREAKING) Migrate `esm-fast-data-entry` to use routes.json
    
    * Bump openmrs and openmrs-esm-framework
    
    * Add missing `css-loader` dependency
    
    * Updated openmrs dependency to 5.0.2
    
    * swc-node dependency resolved
    
    ---------
    
    Co-authored-by: Dennis Kigen <kigen.work@gmail.com>
    Co-authored-by: Vineet Sharma <sharmava05@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-/**
- * This is the entrypoint file of the application. It communicates the
- * important features of this microfrontend to the app shell. It
- * connects the app shell to the React application(s) that make up this
- * microfrontend.
- */
-
 import {
   getAsyncLifecycle,
   defineConfigSchema,
@@ -12,53 +5,28 @@ import {
 } from "@openmrs/esm-framework";
 import { configSchema } from "./config-schema";
 
-declare let __VERSION__: string;
-// __VERSION__ is replaced by Webpack with the version from package.json
-const version = __VERSION__;
+const moduleName = "@openmrs/esm-fast-data-entry-app";
 
-/**
- * This tells the app shell how to obtain translation files: that they
- * are JSON files in the directory `../translations` (which you should
- * see in the directory structure).
- */
-const importTranslation = require.context(
+const options = {
+  featureName: "fast-data-entry-app",
+  moduleName,
+};
+
+export const importTranslation = require.context(
   "../translations",
   false,
   /.json$/,
   "lazy"
 );
 
-/**
- * This tells the app shell what versions of what OpenMRS backend modules
- * are expected. Warnings will appear if suitable modules are not
- * installed. The keys are the part of the module name after
- * `openmrs-module-`; e.g., `openmrs-module-fhir2` becomes `fhir2`.
- */
-const backendDependencies = {
-  fhir2: "^1.2.0",
-  "webservices.rest": "^2.2.0",
-};
+export const root = getAsyncLifecycle(() => import("./Root"), options);
 
-/**
- * This function performs any setup that should happen at microfrontend
- * load-time (such as defining the config schema) and then returns an
- * object which describes how the React application(s) should be
- * rendered.
- *
- * In this example, our return object contains a single page definition.
- * It tells the app shell that the default export of `greeter.tsx`
- * should be rendered when the route matches `hello`. The full route
- * will be `openmrsSpaBase() + 'hello'`, which is usually
- * `/openmrs/spa/hello`.
- */
-function setupOpenMRS() {
-  const moduleName = "@openmrs/esm-fast-data-entry-app";
+export const formsAppMenuLink = getAsyncLifecycle(
+  () => import("./forms-app-menu-link"),
+  options
+);
 
-  const options = {
-    featureName: "fast-data-entry-app",
-    moduleName,
-  };
-
+export function startupApp() {
   defineConfigSchema(moduleName, configSchema);
 
   registerBreadcrumbs([
@@ -68,24 +36,4 @@ function setupOpenMRS() {
       parent: `${window.spaBase}/home`,
     },
   ]);
-
-  return {
-    pages: [
-      {
-        load: getAsyncLifecycle(() => import("./Root"), options),
-        route: "forms",
-      },
-    ],
-    extensions: [
-      {
-        name: "forms-app-link",
-        slot: "app-menu-slot",
-        load: getAsyncLifecycle(() => import("./forms-app-menu-link"), options),
-        online: true,
-        offline: true,
-      },
-    ],
-  };
 }
-
-export { backendDependencies, importTranslation, setupOpenMRS, version };
