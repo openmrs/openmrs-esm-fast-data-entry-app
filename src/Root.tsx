e16b0d96082e89425a4097b74489d30268a86ccf
Kingsley Sabo Joseph <106545931+Eshogheme@users.noreply.github.com>
2025-04-07 14:08:02 -1100

    Update Root.tsx
     added a 404 page to the Root.tsx  in the src directory and also added a buttonin the component

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,4 +1,7 @@
+
 import React from 'react';
+
+import {PageNotFound} from './PageNotFound';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { appPath } from './constant';
 const FormsPage = React.lazy(() => import('./forms-page'));
@@ -13,6 +16,7 @@ const Root = () => {
           <Route path="/" element={<FormsPage />} />
           <Route path="/form/:formUuid" element={<FormEntryWorkflow />} />
           <Route path="/groupform/:formUuid" element={<GroupFormEntryWorkflow />} />
+          <Route path="*" element={<PageNotFound/>} />
         </Routes>
       </BrowserRouter>
     </main>
